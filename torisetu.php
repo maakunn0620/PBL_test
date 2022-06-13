40f350ff5f6d11c59c7efa54f6e1a8ef993b2311
maakunn0620 <106715558+maakunn0620@users.noreply.github.com>
2022-06-13 13:39:43 +0900

    a

--- torisetu.php
+++ torisetu.php
@@ -1,7 +1,9 @@
 //前提条件//
 必ずpullでファイル更新後に作業する
 git clone で作成されたファイルにcdで移動後にコマンド操作する
+PCで別のブランチのデータ作業がしたい場合ブランチを移動しなければいけない
 
+//データの更新方法//
 git pull origin main
 git add .
 git commit -m "a"
@@ -15,3 +17,15 @@ git add .（index.phpとファイルを指定するか.で全部を指定する�
 git commit -m "更新する詳細やコメントを記入"
 リモートにローカルのデータをコピーする
 git push origin main(コピーしたいブランチcd)
+
+//ブランチの移動//
+git checkout main
+
+ブランチの移動
+git checkout main(移動したいブランチ)
+
+//役立つコマンド//
+今いるブランチの確認
+git branch
+データの流れの確認
+git status
