4544718902ceebb917e336099743f0096fbee8b3
kengo11111 <y.kengo2001@icloud.com>
2022-06-17 14:59:14 +0900

    yosioka

--- app/Models/job_area.php
+++ app/Models/job_area.php
@@ -4,13 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Job_area extends Model
 {
-    public function insert(array $jobarea){
-        DB::insert("insert into job_area (job_no, area_no)
-                    values (:job_no,:area_no)", $jobarea);
-    }
+    use HasFactory;
 }
-
