98c071fd9ce1a6c95debbbd22b3ac7911a50c3fa
Nstn131129 <n.nishitani1324@gmail.com>
2022-06-21 11:34:22 +0900

    job_area更新

--- app/Models/job_area.php
+++ app/Models/job_area.php
@@ -4,16 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Job_area extends Model
 {
-    public function insert(array $jobarea){
-        DB::insert("insert into job_area (job_no, area_no)
-                    values (:job_no,:area_no)", $jobarea);
-    }
-    public function init_update_a(string $init_no){
-        DB::delete("DELETE FROM job_area WHERE job_no = ".$init_no);
-    }
+    use HasFactory;
 }
-
