68f306f9455014d081237b1cc56dadb1fb08e011
Nstn131129 <n.nishitani1324@gmail.com>
2022-06-21 12:52:48 +0900

    serchcontrollerの変更

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,11 +14,4 @@ class SearchController extends Controller
         $job_no  = $Department->search();
         return view('list',compact('job_no'));
     }
-
-    public function a()
-    {
-        $Department = new Department;
-        $job_no  = $Department->search();
-        return view('data_list',compact('job_no'));
-    }
 }
